0227a041b9de089286019c1cba36f0f231f75c7d
Admin <Admin@LAPTOP-TFTR3SH7>
2021-08-10 14:49:47 +0530

    wrong

---
+++ src/teenu/Classteenu.java
@@ -0,0 +1,11 @@
+package teenu;
+
+public class Classteenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" github ");
+System.out.println("pimpalshende");
+	}
+
+}
